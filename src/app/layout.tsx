75f105095ac7035b168043d5524589fc2a1b4be9
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-06-28 17:54:44 +0530

    added posthog provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
+import { PostHogProvider } from '@/components/providers/PostHogProvider'
 
 const inter = Inter({ subsets : ['latin'] })
 
@@ -22,11 +23,13 @@ export default function RootLayout({
       className={`h-full antialiased dark`}
     >
       <body className={`${inter.className} bg-[#0a0a0a] text-white min-h-screen flex flex-col`}>
-        <Navbar />
-        <main className="flex-1">
-          {children}
-        </main>
-        <Footer />
+        <PostHogProvider>
+          <Navbar />
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </PostHogProvider>
       </body>
     </html>
   );
