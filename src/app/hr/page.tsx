0d2be9cb0eb71cfd68364dc3faa6d4fa1900826f
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-08-10 15:06:53 +0530

    fixed import issue

--- src/app/hr/page.tsx
+++ src/app/hr/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/lib/supabase-server';
+import { createClient } from '@/lib/supabase/server';
 import { HRList } from '@/components/questions/hr/HRList';
 
 export const metadata = {title: 'HR & Behavioral — CrackIt'}
