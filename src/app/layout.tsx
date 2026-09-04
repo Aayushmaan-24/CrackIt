1957123602f77d7885278b42c905f99fd34fd8f2
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-09-04 20:02:48 +0530

    new font used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import localFont from 'next/font/local'
+import { Inter, Outfit, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
 import { PostHogProvider } from '@/components/providers/PostHogProvider'
 
-const inter = Inter({ 
+const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
 })
 
-const calSans = localFont({
-  src: '../fonts/CalSans-SemiBold.woff2',
-  variable: '--font-cal',
-  display: 'swap',
+const outfit = Outfit({
+  subsets: ['latin'],
+  variable: '--font-outfit',
+})
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
 })
 
 export const metadata: Metadata = {
@@ -32,7 +35,7 @@ export default function RootLayout({
       lang="en"
       className={`h-full antialiased dark`}
     >
-      <body className={`${inter.variable} ${calSans.variable} font-sans bg-[#0a0a0a] text-white min-h-screen flex flex-col`}>
+      <body className={`${inter.variable} ${outfit.variable} ${jetbrainsMono.variable} font-sans bg-[#0a0a0a] text-white min-h-screen flex flex-col`}>
         <PostHogProvider>
           <Navbar />
           <main className="flex-1">
